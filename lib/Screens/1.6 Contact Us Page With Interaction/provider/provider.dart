01072a990184da1706bed55ba4bb00d5adf62f40
Admin <baisanehira@gmail.com>
2024-07-16 20:10:52 +0530

    url_launcher

--- lib/Screens/1.6 Contact Us Page With Interaction/provider/provider.dart
+++ lib/Screens/1.6 Contact Us Page With Interaction/provider/provider.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class ContactPageProvider extends ChangeNotifier
-{
-
+class ContactPageProvider extends ChangeNotifier {
   void phoneLauncher() {
     Uri uri = Uri(scheme: 'tel', path: '+91 1234567890');
     launchUrl(uri);
@@ -13,6 +11,7 @@ class ContactPageProvider extends ChangeNotifier
     Uri uri = Uri.parse('mailto: aish@gmail.com');
     launchUrl(uri);
   }
+
   void instaLauncher() {
     Uri uri = Uri.parse('https://www.instagram.com/accounts/login/?hl=en');
     launchUrl(uri);
