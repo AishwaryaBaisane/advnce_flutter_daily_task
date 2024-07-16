7fc485793735a7ee8929ba5e5473d680cb55d037
Admin <baisanehira@gmail.com>
2024-07-16 20:33:01 +0530

    url_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,28 @@ import 'Screens/1.6 Contact Us Page With Interaction/view/homepage.dart';
 bool theme1 = false;
 bool isHomed = false;
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  bool theme = sharedPreferences.getBool('theme') ?? false;
+  isHomed = sharedPreferences.getBool('home') ?? false;
+  theme1 = isHomed;
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ThemeChange(theme),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => OnetimeScreen(isHomed),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ContactPageProvider(),
+        ),
+      ],
+      builder: (context, child) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,19 +41,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ContactPageProvider(),
-
-
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: ContactPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Colors.purple,
+          onPrimary: Colors.green,
+          secondary: Colors.blue,
+          onSecondary: Colors.deepOrangeAccent,
+          error: Colors.red,
+          onError: Colors.redAccent,
+          surface: Colors.white,
+          onSurface: Colors.black,
+        ),
       ),
+      darkTheme: ThemeData(
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Colors.amber,
+          onPrimary: Colors.blue,
+          secondary: Colors.pink,
+          onSecondary: Colors.yellowAccent,
+          error: Colors.red,
+          onError: Colors.redAccent,
+          surface: Colors.black,
+          onSurface: Colors.white,
+        ),
+      ),
+      home: ContactPage(),
     );
   }
 }
-
-
-
-
-
