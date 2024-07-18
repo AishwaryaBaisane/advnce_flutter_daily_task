a7ad62f1ef5fed23d4e78a52c61e97fbf04143c2
Admin <baisanehira@gmail.com>
2024-07-18 11:59:20 +0530

    7/18/2024

--- lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_password.dart
+++ lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_password.dart
@@ -10,16 +10,10 @@ class GalleryBiometricAuthentication extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery
-        .of(context)
-        .size
-        .width;
-    double height = MediaQuery
-        .of(context)
-        .size
-        .height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     GalleryProvider galleryProvider =
-    Provider.of<GalleryProvider>(context, listen: false);
+        Provider.of<GalleryProvider>(context, listen: false);
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -32,14 +26,12 @@ class GalleryBiometricAuthentication extends StatelessWidget {
             'Gallery',
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          shadowColor: Colors.black,
           backgroundColor: Colors.white,
           elevation: 4,
           actions: [
             PopupMenuButton(
               icon: Icon(Icons.more_vert, size: width * 0.08),
-              itemBuilder: (context) =>
-              [
+              itemBuilder: (context) => [
                 PopupMenuItem<int>(
                   value: 0,
                   child: Text('Hide Folders',
@@ -59,78 +51,12 @@ class GalleryBiometricAuthentication extends StatelessWidget {
             ),
           ],
         ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(
-              width * 0.04, height * 0.022, width * 0.04, height * 0.022),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.blue.withOpacity(0.12)),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Albums',
-                          style: TextStyle(
-                              fontSize: width * 0.038,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue.shade700),
-                        ),
-                        Icon(Icons.arrow_drop_down, color: Colors.blue.shade700)
-                      ],
-                    ),
-                  ),
-                  const Spacer(),
-                  Icon(Icons.search, size: width * 0.064),
-                ],
-              ),
-              SizedBox(height: height * 0.02),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Wrap(
-                    spacing: width * 0.04,
-                    children: List.generate(headings.length, (index) =>
-                        Column(
-                          children: [
-                            Container(
-                              height: height * 0.1225,
-                              width: width * 0.27,
-                              margin: EdgeInsets.only(bottom: 5,
-                                  top: (index != 0 && index != 1 && index != 2)
-                                      ? height * 0.022
-                                      : 0),
-                              // decoration: BoxDecoration(
-                              //     image: DecorationImage(
-                              //         image: AssetImage(galleryImages[index]),
-                              //         fit: BoxFit.cover
-                              //     ),
-                              //     borderRadius: BorderRadius.circular(12),
-                              //     color: Colors.blue),
-                            ),
-                            Text(
-                              headings[index],
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: width * 0.038),
-                            ), Text(
-                              galleryProvider.randomNumber().toString(),
-                              style: TextStyle(
-                                  color: Colors.black38,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: width * 0.028),
-                            ),
-                          ],
-                        ),),
-                  ),
-                ),
-              ),
-            ],
-          ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: height * 0.02),
+            Image(image: AssetImage('assets/img/gallary2.jpg'))
+          ],
         ),
       ),
     );
--- lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_screen2.dart
+++ lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_screen2.dart
@@ -5,11 +5,27 @@ class GalleryScreen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          Text('welcome to this page ')
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: height * 0.02),
+            Container(
+              height: height * 0.9 + 20,
+              width: width * 0.9 + 60,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/img/gallary8.jpeg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
