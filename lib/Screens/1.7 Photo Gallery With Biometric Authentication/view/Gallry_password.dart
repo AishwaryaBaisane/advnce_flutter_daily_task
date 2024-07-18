7443d9ac9737f71d09bec36751ed751d7f015150
Admin <baisanehira@gmail.com>
2024-07-18 20:28:50 +0530

    18/7/2024

--- lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_password.dart
+++ lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_password.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../StepperPage.dart';
 import '../Provider/Photo_Gallary.dart';
 
 class GalleryBiometricAuthentication extends StatelessWidget {
@@ -27,7 +28,6 @@ class GalleryBiometricAuthentication extends StatelessWidget {
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
           backgroundColor: Colors.white,
-          elevation: 4,
           actions: [
             PopupMenuButton(
               icon: Icon(Icons.more_vert, size: width * 0.08),
@@ -51,17 +51,90 @@ class GalleryBiometricAuthentication extends StatelessWidget {
             ),
           ],
         ),
-
-
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: height * 0.02),
-            Image(image: AssetImage('assets/img/gallary2.jpg'))
-          ],
-
+        body: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // SizedBox(height: height * 0.08,),
+              SingleChildScrollView(
+                child: Wrap(
+                  spacing: width * 0.04,
+                  children: List.generate(
+                    headings.length,
+                    (index) => Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          height: height * 0.14,
+                          width: width * 0.27,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(galleryImages[index]),
+                                  fit: BoxFit.cover),
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.blue),
+                        ),
+                        Text(
+                          headings[index],
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: width * 0.038),
+                        ),
+                        Text(
+                          galleryProvider.randomNumber().toString(),
+                          style: TextStyle(
+                              color: Colors.black38,
+                              fontWeight: FontWeight.bold,
+                              fontSize: width * 0.028),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+    // SizedBox(height: height * 0.02),
+    // SizedBox(
+    //   height: height * 0.84,
+    //   child: Image(
+    //     image: AssetImage('assets/img/gallary2.jpg'),
+    //     fit: BoxFit.cover,
+    //   ),
+    // ),
   }
 }
+
+List headings = [
+  'Camera',
+  'Family',
+  'Facebook',
+  'WhatsApp',
+  'ScreenShot',
+  'Instagram',
+  'Download',
+  'College',
+  'Nature',
+  'Animals',
+  'Birds',
+  'Car',
+];
+
+List galleryImages = [
+  'assets/img/gallary_img/img1.jpg',
+  'assets/img/gallary_img/img2.jpg',
+  'assets/img/gallary_img/img3.jpg',
+  'assets/img/gallary_img/img4.jpg',
+  'assets/img/gallary_img/img5.jpg',
+  'assets/img/gallary_img/img6.jpg',
+  'assets/img/gallary_img/img7.jpg',
+  'assets/img/gallary_img/img8.jpg',
+  'assets/img/gallary_img/img9.jpg',
+  'assets/img/gallary_img/img10.jpg',
+  'assets/img/gallary_img/img11.jpg',
+  'assets/img/gallary_img/img12.jpg',
+];
--- lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_screen2.dart
+++ lib/Screens/1.7 Photo Gallery With Biometric Authentication/view/Gallry_screen2.dart
@@ -7,20 +7,18 @@ class GalleryScreen2 extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: height * 0.02),
             Container(
-              height: height * 0.9 + 20,
+              height: height * 0.9605,
               width: width * 0.9 + 60,
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/img/gallary8.jpeg'),
-                  fit: BoxFit.cover,
+                  fit: BoxFit.fill,
                 ),
               ),
             ),
